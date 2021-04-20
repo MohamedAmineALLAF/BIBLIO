c21789dc2debe6afec3117e97adad663901038f3
amineallaf <amine.allafi@gmail.com>
2021-04-20 05:26:18 +0200

    mr

--- Statistiques/indexS.php
+++ Statistiques/indexS.php
@@ -30,25 +30,128 @@ $prenom = $_SESSION["prenom"];
             <div class="overview_card-icon">Total de livres empruntés</div>
           </div>
         </div>
-        <div class="main_overview1">     
-          <div class="overview_card1">
-            <div class="overview_card-info1" >3</div>
-            <div class="overview_card-icon1">livres empruntés aujourd'hui</div>
-          </div>
-          <div class="overview_card1">
-            <div class="overview_card-info1">4</div>
-            <div class="overview_card-icon1">livres empruntés cette semaine</div>
-          </div>
-          <div class="overview_card1">
-            <div class="overview_card-info1">123</div>
-            <div class="overview_card-icon1">livres empruntés ce mois</div>
-          </div>
-          <div class="overview_card1">
-            <div class="overview_card-info1" ">12345678</div>
-            <div class="overview_card-icon1">Total de livres empruntés</div>
-          </div>
-        </div>
-        
+</div>
+<h1 >à-propos des livres : </h1>
+<div class="wrap1">
+    <div class="row11">
+        <?php
+        global $val;
+        $stmt=$pdo->prepare('select l.titreLiv,count(*) as nombres
+        from livre l,exemplaire ex,emprunt e
+        where l.ISBN=ex.ISBN
+        and ex.codeBar = e.codeBar
+        group by l.titreLiv
+        order by count(*) desc
+        LIMIT 4;');
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <h3 style="text-align: center;">Les livres les<span style="color: #1DB954;"> plus </span> empruntés : </h3>
+        <table >
+            <tbody>
+                <tr>
+                    <td>Livres</td>
+                    <td>Nombres d'emprunts</td>
+                </tr>
+            <?php foreach ($records as $record):?>
+                <tr>
+                    <td><?= $record['titreLiv'] ?></td>
+                    <td><?= $record['nombres'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="row22">
+    <?php
+        global $val;
+        $stmt=$pdo->prepare('select l.titreLiv,count(*) as nombres
+        from livre l,exemplaire ex,emprunt e
+        where l.ISBN=ex.ISBN
+        and ex.codeBar = e.codeBar
+        group by l.titreLiv
+        order by count(*) asc
+        LIMIT 4;');
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <h3 style="text-align: center;">Les livres les <span style="color: red;"> moins </span> empruntés : </h3>
+        <table >
+            <tbody>
+                <tr>
+                    <td>Livres</td>
+                    <td>Nombres d'emprunts</td>
+                </tr>
+            <?php foreach ($records as $record):?>
+                <tr>
+                    <td><?= $record['titreLiv'] ?></td>
+                    <td><?= $record['nombres'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+</div>
+<h1  >à-propos des disciplines : </h1>
+<div class="wrap1">
+    <div class="row11">
+        <?php
+        global $val;
+        $stmt=$pdo->prepare('
+        select DISTINCT d.libelleDis
+        from livre l,exemplaire ex,emprunt e,discipline d
+        where l.ISBN=ex.ISBN
+        and ex.codeBar = e.codeBar
+        AND d.codeDis = l.codeDis
+        group by l.titreLiv
+        order by count(*) desc
+        LIMIT 4;');
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+         <h3 style="text-align: center;">Les disciplines les<span style="color: #1DB954;"> plus </span> empruntés : </h3>
+        <table >
+            <tbody>
+                <tr>
+                    <td>Discipline</td>
+                </tr>
+            <?php foreach ($records as $record):?>
+                <tr>
+                    <td><?= $record['libelleDis'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
+    <div class="row22">
+    <?php
+        global $val;
+        $stmt=$pdo->prepare('
+        select DISTINCT d.libelleDis
+        from livre l,exemplaire ex,emprunt e,discipline d
+        where l.ISBN=ex.ISBN
+        and ex.codeBar = e.codeBar
+        AND d.codeDis = l.codeDis
+        group by l.titreLiv
+        order by count(*) asc
+        LIMIT 4;');
+        $stmt->execute();
+        $records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        ?>
+        <h3 style="text-align: center;">Les livres les plus empruntés : </h3>
+        <table >
+            <tbody>
+                <tr>
+                    <td>Discipline</td>
+                </tr>
+            <?php foreach ($records as $record):?>
+                <tr>
+                    <td><?= $record['libelleDis'] ?></td>
+                </tr>
+            <?php endforeach; ?>
+            </tbody>
+        </table>
+    </div>
 </div>
 
 <?=template_footer()?>
